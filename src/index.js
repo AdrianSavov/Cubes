a01d72bc284bd75be370e96ec8c610001ff271bd
AdrianSavov <skandalno@icloud.com>
2023-05-29 14:10:08 +0300

    Added static files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('src/static'))
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.use('/', (req, res) => {
-    res.send('ZDR BEPCE')
+    res.render('index')
 });
 
 app.listen(PORT, () => console.log('Server is listening...'))
