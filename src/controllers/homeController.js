c45a0806e92951f68380d749c5515a719cf2df43
AdrianSavov <skandalno@icloud.com>
2023-05-30 16:52:25 +0300

    Add 404 page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,9 +10,9 @@ router.get('/about', (req, res) => {
     res.render('about')
 });
 
-// router.get('*', (req, res) => {
-//     res.render('404')
-// });
+router.get('/404', (req, res) => {
+    res.render('404')
+});
 
 
 
