e74d5c4aee82f4da2560048f8bb77f5356d18121
AdrianSavov <skandalno@icloud.com>
2023-05-30 12:29:53 +0300

    Add cube controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,9 +8,8 @@ router.get('/about', (req, res) => {
     res.render('about')
 });
 
-
-router.get('*', (req, res) => {
-    res.render('404')
-});
+// router.get('*', (req, res) => {
+//     res.render('404')
+// });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 
 expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 
 
