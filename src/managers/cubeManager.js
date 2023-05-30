b1e312fe0a5c22821af31f639bd7764a9b96b0f9
AdrianSavov <skandalno@icloud.com>
2023-05-30 17:29:52 +0300

    Add search engine

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -5,9 +5,30 @@ const cubes = [{
     imageUrl: 'nqma',
     difficultyLevel: 17,
     id: uniqid(),
+},
+{
+    name: 'NISHTO',
+    description: 'UJAST',
+    imageUrl: 'nqma',
+    difficultyLevel: 3,
+    id: uniqid(), 
 }];
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+    if (from) {
+        result = result.filter(cube => Number(cube.difficultyLevel) >= Number(from));
+    }
+    if (to) {
+        result = result.filter(cube => Number(cube.difficultyLevel) <= Number(to));
+
+    }
+    return result;
+}
 exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId)
 exports.create = (name, description, imageUrl, difficultyLevel) => {
 
