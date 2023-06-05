022af163926c7957b4da659a976fc2c542ee41f1
AdrianSavov <skandalno@icloud.com>
2023-06-05 14:33:06 +0300

    Show accessories attach

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const cubeManager = require('../managers/cubeManager')
+const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
    
@@ -31,8 +32,11 @@ router.get('/:cubeId/details', async (req, res) => {
     res.render('details', { cube });
 })
 
-router.get('/:cubeId/accessories/attach', (req, res) => {
-    res.render('accessory/attach')
+router.get('/:cubeId/accessories/attach', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+    console.log(accessories);
+    res.render('accessory/attach', { cube, accessories });
 })
 
 module.exports = router;
